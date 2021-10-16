990d1b1b486670fe3742d62b9e10ba8cc6361e81
Hassan Irfan <83138580+Hassan837@users.noreply.github.com>
2021-10-16 05:44:53 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,16 +2,35 @@ let addToDoButton = document.getElementById('addToDo');
 let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
 
+// funtion for empty input
+function inputLength (){
+    return inputField.value.length;
+}
+
+
+// fintion for adding input
 addToDoButton.addEventListener('click', function(){
-    var paragraph = document.createElement('p');
-    paragraph.classList.add('paragraph-styling');
-    paragraph.innerText = inputField.value;
-    toDoContainer.appendChild(paragraph);
-    inputField.value = "";
-    paragraph.addEventListener('click', function(){
-        paragraph.style.textDecoration = "line-through";
-    })
-    paragraph.addEventListener('dblclick', function(){
-        toDoContainer.removeChild(paragraph);
-    })
+
+    if (inputLength() > 0){
+        var paragraph = document.createElement('p');
+        paragraph.innerHTML = inputField.value;
+        toDoContainer.appendChild(paragraph);
+        paragraph.classList.add('afterinputText');
+
+        var btn = document.createElement('button');
+        btn.appendChild(document.createTextNode("Delete"));
+        paragraph.appendChild(btn);
+        btn.classList.add('afterClick');
+
+        inputField.value = "" ;  
+    }
+
+    else{
+        alert ("Kindly fill the Task");
+    }
+
+// funtion for deleting
+btn.addEventListener('click', function(){
+    toDoContainer.removeChild(paragraph);
+})
 })
